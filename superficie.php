c4ab9b7e36d7e65c4740e899c08e5ea651a65b7f
Felipe Siqueira <felipesiqueira1212@gmail.com>
2019-10-15 18:17:01 -0300

    Resolução Ex 2b)

--- superficie.php
+++ superficie.php
@@ -4,3 +4,11 @@
         return $superficie;
     }
     echo "A área de superície do triângulo é de: ".triangulo(3,8)." metros.";
+
+    echo "<br>"."<br>";
+
+    function retangulo($base,$altura){
+        $superficie=($base*$altura);
+        return $superficie;
+    }
+    echo "A área de superície do retângulo é de: ".retangulo(3,8)." metros.";
